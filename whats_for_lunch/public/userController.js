2e306abc0d333fef58d2ccd85e34e6755644462d
Jeffrey Macks <jmacks1988@gmail.com>
2015-12-07 18:50:26 -0500

    four square api url now has param which pulls random cuisine types

--- whats_for_lunch/public/userController.js
+++ whats_for_lunch/public/userController.js
@@ -70,13 +70,20 @@ self.getFood = getFood;
 getFood();
 
 function getFood(){
+  var cuisineArray = ['sushi', 'pizza', 'diner', 'american', 'chinese', 'korean', 'tapas', 'sandwich', 'mediterranean', 'burger', 'coffee', 'french', 'italian', 'german', 'spanish', 'mexican'];
+  var cuisineLength = cuisineArray.length;
+  var randomInCuisineArray = Math.floor(Math.random()*cuisineLength);
+  var randomCuisine = cuisineArray[randomInCuisineArray];
 $http
-.get("https://api.foursquare.com/v2/venues/search?client_id=D3SET0CHYGC3CASAHZN2KNTLUSQKA0KQMIXARVBKTN5PXXBM&client_secret=P32CRHV34KCIRUB0GAEZAB54NQBVJ3K42R0WC0DKF5MICCUX&v=20130815&ll=40.7,-74&query=sushi")
+.get("https://api.foursquare.com/v2/venues/search?client_id=D3SET0CHYGC3CASAHZN2KNTLUSQKA0KQMIXARVBKTN5PXXBM&client_secret=P32CRHV34KCIRUB0GAEZAB54NQBVJ3K42R0WC0DKF5MICCUX&v=20130815&ll=40.7,-74&query=" + randomCuisine)
+
 .then(function(res){
   var venuesLength = res.data.response.venues.length
   var rando = Math.floor(Math.random()* venuesLength)
   console.log(res.data.response.venues[rando].name);
   self.all = res.data.response.venues[rando].name;
+  // self.all = res.data.response.venues.name;
+
  // self.all = res.response;
 });
 }
